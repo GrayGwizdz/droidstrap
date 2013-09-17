f4666852b02622ce6f97aa363a22f765502b1b96
Gray Gwizdz <gray.gwizdz@gmail.com>
2013-09-16 23:58:21 -0400

    Explanation of methods/functions

--- assets/files/js/android.js
+++ assets/files/js/android.js
@@ -1,7 +1,9 @@
+/* Function called in index.html */
 function javascriptToast(val) {
 	window.JavascriptHandler.handlerToast(val);
 }
 
+/* Function called in index.html */
 function javascriptIntent() {
 	window.JavascriptHandler.handlerIntent();
 }
--- src/com/gray/gwizdz/example/droidstrap/JavascriptHandler.java
+++ src/com/gray/gwizdz/example/droidstrap/JavascriptHandler.java
@@ -4,17 +4,21 @@ import android.util.Log;
 import android.webkit.JavascriptInterface;
 
 public class JavascriptHandler {
+	
 	MainActivity parentActivity;
+	
     public JavascriptHandler(MainActivity activity)  {
         parentActivity = activity;
     }
     
+    /** Method called by android.js */
     @JavascriptInterface
     public void handlerToast(String val){
         Log.d("mylog","JavascriptHandler.handlerToast is called : " + val);
         this.parentActivity.activityToast(val);
     }
     
+    /** Method called by android.js */
     @JavascriptInterface
     public void handlerIntent(){
         Log.d("mylog","JavascriptHandler.handlerIntent is called.");
